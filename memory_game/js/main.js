63499a4b7ee967e737ecb4abb63e369cb9c815d7
Nerko <Nerkosokolic@gmail.com>
2019-05-27 19:46:23 +1000

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+console.log("User flipped a " + cardFour);
+console.log("User flipped a " + cardOne);
